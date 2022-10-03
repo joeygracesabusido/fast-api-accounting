af0eff969d5c9957ba1b494c12b49f83a54f954e
unknown <joeysabusido.@gmail.com>
2022-10-03 20:21:38 +0300

    Delete Headers for Authentication

--- routes/rizal_project.py
+++ routes/rizal_project.py
@@ -51,7 +51,7 @@ def validateLogin(request:Request):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail= "Not Authorized please Login",
-            headers={"WWW-Authenticate": "Basic"},
+            # headers={"WWW-Authenticate": "Basic"},
         )
 
 from schemas.rizal import Equipment,Equipments 
