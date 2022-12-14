2c0ac6daf94e067d590f6959b07ae806e5b2236b
unknown <joeygracesabusido@gmail.com>
2022-12-14 17:35:37 +0800

    12.14.22 5:35PM

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from routes.client import client
 from routes.zamboanga_client import zamboanga_client
 from routes.rizal_project import rizal_project
 from routes.employee_user import employee_user
+from config.vitaliDB import vitali
+
+from mysql.connector import connect
 
 # from config.database import database
 
@@ -28,6 +31,9 @@ app.include_router(client)
 app.include_router(zamboanga_client)
 app.include_router(rizal_project)
 app.include_router(employee_user)
+app.include_router(vitali)
+
+
 
 
 # @app.on_event("startup")
